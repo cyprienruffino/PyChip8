a923591b556bd5ebefc641b9497c6e15bfe1fa94
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2017-02-27 20:00:33 +0100

    Setup the hooks API

---
+++ emulator/hooks_api/hook.py
@@ -0,0 +1,9 @@
+
+
+class Hook:
+
+    def __init__(self, name:str):
+        raise "Abstract class, cannot be instanciated"
+
+    def call(self):
+        raise "Abstract class, cannot be instanciated"
