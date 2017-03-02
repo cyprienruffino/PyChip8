827a5be9d848271a3d099c600c216e64538d167e
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2017-03-02 12:04:39 +0100

    Woops, my bad, this was bad typing

--- emulator/Controller.py
+++ emulator/Controller.py
@@ -10,14 +10,15 @@ from view.ISound import ISound
 class Controller:
     def __init__(self):
 
-        self.emulator:Emulator=Emulator()
-        self.gfx:dict[str,IGraphics] = dict()
-        self.sound:dict[str,ISound] = dict()
-        self.controls:dict[str,IControls] = dict()
+        self.__emulator:Emulator=Emulator()
 
-        self.pre_cycle_hooks:dict[str, IHook] = dict()
-        self.post_cycle_hooks:dict[str, IHook] = dict()
-        self.init_hooks:dict[str, IHook] = dict()
+        self.__gfx:dict = dict()
+        self.__sound:dict = dict()
+        self.__controls:dict = dict()
+
+        self.__pre_cycle_hooks:dict = dict()
+        self.__post_cycle_hooks:dict = dict()
+        self.__init_hooks:dict = dict()
 
         self.__looping_forwards:bool = False
         self.__looping_backwards:bool = False
@@ -34,22 +35,22 @@ class Controller:
     #### Private
 
     def __call_graphics(self):
-       for _,v in self.gfx.items():
-            if self.emulator.draw_flag:
-                v.draw(self.emulator.gfx_pixels)
+       for _,v in self.__gfx.items():
+            if self.__emulator.draw_flag:
+                v.draw(self.__emulator.gfx_pixels)
 
     def __call_controls(self):
-        for _, v in self.controls.items():
+        for _, v in self.__controls.items():
             for i in v.get_keys_pressed():
-                self.emulator.press_key(i)
+                self.__emulator.press_key(i)
 
             for i in v.get_keys_released():
-                self.emulator.release_key(i)
+                self.__emulator.release_key(i)
 
 
     def __call_sound(self):
-        for _, v in self.sound.items():
-            if self.emulator.beep_flag:
+        for _, v in self.__sound.items():
+            if self.__emulator.beep_flag:
                 v.beep()
 
     def __start_cycle_timer(self):
@@ -61,15 +62,15 @@ class Controller:
             time.sleep((1/60) - elapsed)
 
     def __call_init_hooks(self) -> None:
-        for _, v in self.init_hooks.items():
+        for _, v in self.__init_hooks.items():
             v.call()
 
     def __call_pre_hooks(self) -> None:
-        for _, v in self.pre_cycle_hooks.items():
+        for _, v in self.__pre_cycle_hooks.items():
             v.call()
 
     def __call_post_hooks(self) -> None:
-        for _, v in self.post_cycle_hooks.items():
+        for _, v in self.__post_cycle_hooks.items():
             v.call()
 
     def __start(self):
@@ -79,40 +80,40 @@ class Controller:
     #### Modules
 
     def add_gfx(self, name:str, gfx: IGraphics) -> None:
-        self.gfx[name] = gfx
+        self.__gfx[name] = gfx
 
     def add_sound(self, name:str, sound: ISound) -> None:
-        self.sound[name]=sound
+        self.__sound[name]=sound
 
     def add_controls(self, name:str, controls: IControls) -> None:
-        self.controls[name] = controls
+        self.__controls[name] = controls
 
     #### Hooks
 
     def add_init_hook(self, name: str, function) -> None:
-        self.init_hooks[name] = function
+        self.__init_hooks[name] = function
 
     def add_pre_cycle_hook(self, name: str, function) -> None:
-        self.pre_cycle_hooks[name] = function
+        self.__pre_cycle_hooks[name] = function
 
     def add_post_cycle_hook(self, name: str, function) -> None:
-        self.post_cycle_hooks[name] = function
+        self.__post_cycle_hooks[name] = function
 
     def remove_init_hook(self, name: str) -> bool:
-        if self.init_hooks[name]:
-            del self.init_hooks[name]
+        if self.__init_hooks[name]:
+            del self.__init_hooks[name]
             return True
         return False
 
     def remove_pre_cycle_hook(self, name: str) -> bool:
-        if self.pre_cycle_hooks[name]:
-            del self.pre_cycle_hooks[name]
+        if self.__pre_cycle_hooks[name]:
+            del self.__pre_cycle_hooks[name]
             return True
         return False
 
     def removePostcycleHook(self, name: str) -> bool:
-        if self.post_cycle_hooks[name]:
-            del self.post_cycle_hooks[name]
+        if self.__post_cycle_hooks[name]:
+            del self.__post_cycle_hooks[name]
             return True
         return False
 
@@ -129,7 +130,7 @@ class Controller:
                 byte = f.read(1)
                 i += 1
 
-        self.emulator.load_rom(rom)
+        self.__emulator.load_rom(rom)
 
     def step(self) -> None:
         if not self.__started:
@@ -140,7 +141,7 @@ class Controller:
         self.__call_graphics()
         self.__call_controls()
         self.__call_sound()
-        self.emulator.gamestep()
+        self.__emulator.gamestep()
         self.__call_post_hooks()
 
 
@@ -149,7 +150,7 @@ class Controller:
         self.__call_graphics()
         self.__call_controls()
         self.__call_sound()
-        self.emulator.gamestep_backwards()
+        self.__emulator.gamestep_backwards()
         self.__call_post_hooks()
 
 
