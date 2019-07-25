e01d1854d04ecd9100a385a723dfa076d05c742d
Cyprien Ruffino <ruffino.cyprien@gmail.com>
2019-07-25 18:51:02 +0200

    Moving things around

---
+++ controls/CursesControls.py
@@ -0,0 +1,15 @@
+from emulator.view.IControls import IControls
+
+
+class ControlsStub(IControls):
+
+    def __init__(self):
+        pass
+
+    def get_keys_pressed(self) -> list:
+        return []
+
+    def get_keys_released(self) -> list:
+        return []
+
+
---
+++ display/PoorDisplay.py
@@ -0,0 +1,21 @@
+from emulator.view.IGraphics import IGraphics
+
+
+class PoorGraphics(IGraphics):
+    def __init__(self):
+        pass
+
+    def open_view(self) -> None:
+        pass
+
+    def draw(self, gfx: bytearray) -> None:
+        line = ""
+        for y in range(0, 32):
+            for x in range(0, 64):
+                if gfx[x]:
+                    line += '#'
+                else:
+                    line += " "
+            print(line)
+            line = ""
+        print("-" * 64)
