259dc2fe07b317091669a3854df37131001acad4
Cyprien Ruffino <ruffino.cyprien@gmail.com>
2019-07-26 02:02:51 +0200

    Changes to the main for debug

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
 #!/usr/bin/env python
 
-from api.API import API
 from emulator.Controller import Controller
-from sample_hooks.OpcodeHook import OpcodeHook
-from sample_hooks.HelloWorldHook import HelloWorldHook
-from emulator.view.stub.ControlsStub import ControlsStub
-from emulator.view.stub.SoundStub import SoundStub
-from emulator.view.testgfx.Poorgfx import PoorGraphics
-
+from display.CursesDisplay import CursesDisplay
+from display.PoorDisplay import PoorGraphics
 
 def run_emulator():
     controller = Controller()
-    controller.add_sound("stub", SoundStub())
-    controller.add_gfx("poor", PoorGraphics())
-    # controller.add_gfx("stub", GraphicsStub())
-    controller.add_controls("stub", ControlsStub())
+    controller.add_display("curses", CursesDisplay())
+    #controller.add_display("poor", PoorGraphics())
     controller.set_frame_limit(True)
 
-    api = API(controller)
+    # api = API(controller)
     # controller.add_init_hook("helloworld", api.create_hook(HelloWorldHook))
     # controller.add_pre_cycle_hook("opcode", api.create_hook(OpcodeHook))
-    # controller.add_pre_frame_hook("opcode", api.create_hook(OpcodeHook))
 
     controller.load_rom("ROMs/TETRIS.bin")
-    for i in range(0, 300):
-        controller.next_frame()
-
-    controller.start_looping_forwards()
-
+    #controller.load_rom("ROMs/Puzzle.ch8")
+    #for i in range(1000): controller.next_frame()
+    controller.start()
 
 if __name__ == "__main__":
     run_emulator()
