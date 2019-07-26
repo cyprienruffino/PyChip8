88b9d8d9e26ee13bfed51a0ab9cbaa74f6fcd009
Cyprien Ruffino <ruffino.cyprien@gmail.com>
2019-07-26 15:30:32 +0200

    Editing main and README

--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 The goal here is to create a flexible CHIP-8 emulator with hooks and tools to experiment with emulation and hopefully create a machine learning environment.
 
 
+# Usage
+$ python main.py path_to_rom
+
+# Keybindings
+| | | | | | | | | |
+|---|---|---|---|---|---|---|---|---|
+|1|2|3|C|/|1|2|3|4|
+|4|5|6|D|/|a|z|e|r|
+|7|8|9|E|/|q|s|d|f|
+|A|0|B|F|/|w|x|c|v|
--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
+import sys
 
 from emulator.Controller import Controller
 from display.CursesDisplay import CursesDisplay
-from display.PoorDisplay import PoorGraphics
 
-def run_emulator():
+
+def run_emulator(rom):
     controller = Controller()
     controller.add_display("curses", CursesDisplay())
-    #controller.add_display("poor", PoorGraphics())
     controller.set_frame_limit(True)
 
     # api = API(controller)
     # controller.add_init_hook("helloworld", api.create_hook(HelloWorldHook))
     # controller.add_pre_cycle_hook("opcode", api.create_hook(OpcodeHook))
 
-    controller.load_rom("ROMs/TETRIS.bin")
-    #controller.load_rom("ROMs/Puzzle.ch8")
-    #for i in range(1000): controller.next_frame()
+    controller.load_rom(rom)
     controller.start()
 
+
 if __name__ == "__main__":
-    run_emulator()
+    if len(sys.argv < 2):
+        print("Usage: python main.py rompath")
+        exit(0)
+    run_emulator(sys.argv[1])
