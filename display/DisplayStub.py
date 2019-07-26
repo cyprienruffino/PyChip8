4a0f9c7dd396b59b48177e80d9bddf986f4b5b3a
Cyprien Ruffino <ruffino.cyprien@gmail.com>
2019-07-26 02:03:58 +0200

    Fixing the old display modules

--- display/DisplayStub.py
+++ display/DisplayStub.py
@@ -5,8 +5,5 @@ class DisplayStub(IGraphics):
     def __init__(self):
         pass
 
-    def open_view(self) -> None:
-        pass
-
     def draw(self, gfx: bytearray) -> None:
         pass
--- display/PoorDisplay.py
+++ display/PoorDisplay.py
@@ -1,7 +1,7 @@
 from .IDisplay import IDisplay
 
 
-class PoorGraphics(IGraphics):
+class PoorGraphics(IDisplay):
     def __init__(self):
         pass
 
@@ -12,10 +12,19 @@ class PoorGraphics(IGraphics):
         line = ""
         for y in range(0, 32):
             for x in range(0, 64):
-                if gfx[x]:
-                    line += '#'
+                if gfx[x+64*y]:
+                    line += '##'
                 else:
-                    line += " "
+                    line += "  "
             print(line)
             line = ""
-        print("-" * 64)
+        print("-" * 128)
+
+
+    def get_keys_pressed(self) -> list:
+        return []
+
+    def get_keys_released(self) -> list:
+        return []
+
+
